f1a2198a92458d0ed20897c2444d12d7ee123813
Ensalado <rayagordo@gmail.com>
2026-05-22 09:13:07 +0200

    Reposition exit room banners to closed walls

--- Data/exitRooms.js
+++ Data/exitRooms.js
@@ -151,10 +151,8 @@ export const EXIT_ROOM_TEMPLATES = [
       z: 1.7,
     },
     decorativeModules: [
-      { x: -1, z: -3.48, w: 1, d: 1, moduleId: "banner", rotationY: -Math.PI },
-      { x: 1, z: -3.48, w: 1, d: 1, moduleId: "banner", rotationY: -Math.PI },
-      { x: -1, z: 3.48, w: 1, d: 1, moduleId: "banner", rotationY: 0 },
-      { x: 1, z: 3.48, w: 1, d: 1, moduleId: "banner", rotationY: 0 },
+      { x: -3.48, z: 0, w: 1, d: 1, moduleId: "banner", rotationY: Math.PI / 2 },
+      { x: 3.48, z: 0, w: 1, d: 1, moduleId: "banner", rotationY: -Math.PI / 2 },
       { x: -1, z: 2.5, w: 1, d: 1, moduleId: "floorDetail" },
       { x: 2.5, z: -2, w: 1, d: 1, moduleId: "stones", rotationY: Math.PI },
     ],
@@ -312,8 +310,8 @@ export const EXIT_ROOM_TEMPLATES = [
       z: 0,
     },
     decorativeModules: [
-      { x: -2.6, z: -3.48, w: 1, d: 1, moduleId: "banner", rotationY: Math.PI / 2 },
-      { x: -2.6, z: 3.48, w: 1, d: 1, moduleId: "banner", rotationY: Math.PI / 2 },
+      { x: 0, z: -2.48, w: 1, d: 1, moduleId: "banner", rotationY: Math.PI },
+      { x: 0, z: 2.48, w: 1, d: 1, moduleId: "banner", rotationY: 0 },
       { x: -1.6, z: 1.8, w: 1, d: 1, moduleId: "floorDetail" },
       { x: -1.9, z: -2, w: 1, d: 1, moduleId: "stones", rotationY: Math.PI },
     ],
