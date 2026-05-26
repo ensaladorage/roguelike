983e0d7d59cb33c5bcd23dc796a95b9f80fac9f9
Ensalado <rayagordo@gmail.com>
2026-05-26 21:14:28 +0200

    Add four-way treasure room and test floor

--- Data/handcraftedLevels.js
+++ Data/handcraftedLevels.js
@@ -2,7 +2,7 @@ export const HANDCRAFTED_LEVELS = [
   {
     kind: "assembled",
     tileSetId: "scenarioDefault",
-    name: "Test Floor: enter_room_north_south_01 + combat_02 + exit_room_north_south_01",
+    name: "Test Floor: enter_room_north_south_01 + treasure_01 + exit_room_north_south_01",
     connectorStyleId: "openCorridor",
     decorationFill: {
       // Cambia a un numero fijo para repetir la misma distribucion mientras ajustas a mano.
@@ -65,25 +65,25 @@ export const HANDCRAFTED_LEVELS = [
         },
       ],
     },
-    playerStart: { x: -8, z: -13 },
-    floorSize: 54,
+    playerStart: { x: 0, z: -13 },
+    floorSize: 40,
     rooms: [
       {
         id: "EnterRoom",
         templateId: "enter_room_north_south_01",
-        position: { x: -8, z: -11 },
+        position: { x: 0, z: -11 },
         rotationY: 0,
       },
       {
-        id: "CombatRoom",
-        templateId: "combat_02",
-        position: { x: 0, z: 5 },
+        id: "TreasureRoom",
+        templateId: "treasure_01",
+        position: { x: 0, z: 0 },
         rotationY: 0,
       },
       {
         id: "ExitRoom",
         templateId: "exit_room_north_south_01",
-        position: { x: 8, z: -10 },
+        position: { x: 0, z: 10 },
         rotationY: 0,
       },
     ],
